b2e0b933e8505c85ef5cabb6924b250fc14df1ee
inteltoasters <inteltoasters@getyomail.cc>
2025-09-24 17:07:50 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "Downloading the script..."
+
+mkdir -p /usr/bin/WADE
+
+wget -O /usr/bin/WADE/shell.sh https://raw.githubusercontent.com/inteltoasters/WADE/refs/heads/main/shell.sh
+
+echo "Adding the x-session..."
+
+cat <<HEREDOC > /usr/share/xsessions/WADE.desktop
+[Desktop Entry]
+Version=1.0
+Name=WADE
+Comment=Michaelsoft Binbows
+Exec=/usr/bin/WADE/shell.sh
+Icon=
+Type=Application
+HEREDOC
+
+echo "All done"
